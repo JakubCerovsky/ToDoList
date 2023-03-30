a5e78749b0f16cce339b3d523316e50fddbeaaab
Jakub Cerovský <jaku1198@edu.zealand.dk>
2023-03-30 22:18:07 +0200

    Converted to Blazor Web Application

---
+++ HttpClients/ClientInterfaces/IToDoService.cs
@@ -0,0 +1,15 @@
+﻿using Models.DTOs;
+using Models.Models;
+
+namespace HttpClients.ClientInterfaces;
+
+public interface IToDoService
+{
+    Task CreateAsync(ToDoCreationDTO dto);
+    Task<ICollection<ToDo>> GetAsync(
+        string? userName, 
+        int? userId, 
+        bool? completedStatus, 
+        string? titleContains
+    );
+}
---
+++ HttpClients/Implementations/ToDoHttpClient.cs
@@ -0,0 +1,74 @@
+﻿using System.Net.Http.Json;
+using System.Text.Json;
+using HttpClients.ClientInterfaces;
+using Models.DTOs;
+using Models.Models;
+
+namespace HttpClients.Implementations;
+
+public class ToDoHttpClient:IToDoService
+{
+    private readonly HttpClient client;
+
+    public ToDoHttpClient(HttpClient client)
+    {
+        this.client = client;
+    }
+
+    public async Task CreateAsync(ToDoCreationDTO dto)
+    {
+        HttpResponseMessage response = await client.PostAsJsonAsync("/todos",dto);
+        if (!response.IsSuccessStatusCode)
+        {
+            string content = await response.Content.ReadAsStringAsync();
+            throw new Exception(content);
+        }
+    }
+    
+    public async Task<ICollection<ToDo>> GetAsync(string? userName, int? userId, bool? completedStatus, string? titleContains)
+    {
+        string query = ConstructQuery(userName, userId, completedStatus, titleContains);
+
+        HttpResponseMessage response = await client.GetAsync("/todos"+query);
+        string content = await response.Content.ReadAsStringAsync();
+        if (!response.IsSuccessStatusCode)
+        {
+            throw new Exception(content);
+        }
+
+        ICollection<ToDo> todos = JsonSerializer.Deserialize<ICollection<ToDo>>(content, new JsonSerializerOptions
+        {
+            PropertyNameCaseInsensitive = true
+        })!;
+        return todos;
+    }
+    
+    private static string ConstructQuery(string? userName, int? userId, bool? completedStatus, string? titleContains)
+    {
+        string query = "";
+        if (!string.IsNullOrEmpty(userName))
+        {
+            query += $"?username={userName}";
+        }
+
+        if (userId != null)
+        {
+            query += string.IsNullOrEmpty(query) ? "?" : "&";
+            query += $"userid={userId}";
+        }
+
+        if (completedStatus != null)
+        {
+            query += string.IsNullOrEmpty(query) ? "?" : "&";
+            query += $"completedstatus={completedStatus}";
+        }
+
+        if (!string.IsNullOrEmpty(titleContains))
+        {
+            query += string.IsNullOrEmpty(query) ? "?" : "&";
+            query += $"titlecontains={titleContains}";
+        }
+
+        return query;
+    }
+}
